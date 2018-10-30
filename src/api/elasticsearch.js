abcebdf72ec1d8a52a7c21d184f4f3e41f3dda4a
Rachel <rachelwshen@gmail.com>
2018-10-30 13:01:44 -0700

    environmental variable and cloud elasticsearch

--- src/api/elasticsearch.js
+++ src/api/elasticsearch.js
@@ -1,16 +1,17 @@
 import { Component } from 'react';
 import elasticsearch from 'elasticsearch';
 
-// 'https://booktracker-elastic.herokuapp.com/'
+//'https://booktracker-elastic.herokuapp.com/'
 
 export const client = new elasticsearch.Client({
-    host: ['https://localhost:9200/', 'http://localhost:9200/'],
-    ssl: {
-        ca: process.env.CERTIFICATE,
-        cert: process.env.CERTIFICATE,
-        key: process.env.KEY,
-        rejectUnauthorized: false
-      },
+    host: [
+            {
+                protocol: 'https',
+                host:'5c9e2ac52bc546d8b58a1b0b5752a0a1.us-west1.gcp.cloud.es.io',
+                port: 9243,
+                auth: process.env.REACT_APP_LOGIN,
+            }
+        ], 
     apiVersion: '6.3'
 });
 
